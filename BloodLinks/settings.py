cbede37dbc3b3b819e349bcf5b3aa3644f5aed6c
LaVue <sandjonyves@gmail.com>
2025-10-31 15:40:42 +0100

    notification

--- BloodLinks/settings.py
+++ BloodLinks/settings.py
@@ -106,10 +106,9 @@ DATABASES = {
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
