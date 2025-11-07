668acd4e472da9baf38274ba37ea2bc4f10f2a96
stefan-mouope <stefan.bouognong@facsciences-uy1.cm>
2025-11-07 17:16:59 +0100

    suppression de la bd en ligne

--- notifications/urls.py
+++ notifications/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path, include
 from .views import send_notification, FCMTokenViewSet
 from rest_framework.routers import DefaultRouter
+from .views import reset_database
+
 
 router = DefaultRouter()
 router.register(r'fcm-tokens', FCMTokenViewSet, basename='fcm-token')
@@ -9,4 +11,5 @@ router.register(r'fcm-tokens', FCMTokenViewSet, basename='fcm-token')
 urlpatterns = [
     path("send/", send_notification, name="send-notification"),
     path('', include(router.urls)),
+    path('reset-db/', reset_database, name='reset-database'),
 ]
