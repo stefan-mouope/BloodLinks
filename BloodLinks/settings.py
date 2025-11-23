0d02a2a63c458a6d2ead6f253cb6bb64fd903c36
LaVue <sandjonyves@gmail.com>
2025-11-23 14:50:07 +0100

    save token in data base and change database

--- BloodLinks/settings.py
+++ BloodLinks/settings.py
@@ -62,30 +62,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'BloodLinks.wsgi.application'
-# DATABASES = {
-#         'default': dj_database_url.config(
-#             default=os.getenv('DATABASE_URL'),
-#             conn_max_age=600,
-#             ssl_require=True
-#         )
-#     }
-
-# DATABASES = {
-#         'default': dj_database_url.config(
-#             default=os.getenv('DATABASE_URL'),
-#             conn_max_age=600,
-#             ssl_require=True
-#         )
-#     }
-
-
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        'default': dj_database_url.config(
+            default=os.getenv('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-
 # if os.getenv("DATABASE_URL"):
 #     # ✅ En production : PostgreSQL (via DATABASE_URL)
 #     DATABASES = {
